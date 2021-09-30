b1e47b37c692d1139c5dff057ed1b61ea8ae58bc
GonzaloGassele <48393196+GonzaloGassele@users.noreply.github.com>
2021-09-30 08:06:32 -0300

    Add files via upload

--- Sistema de vigilancia inteligente/Alert.py
+++ Sistema de vigilancia inteligente/Alert.py
@@ -1,18 +1,8 @@
-from twilio.rest import Client
 from datetime import datetime
 import cv2
 from pathlib import Path
 import pywhatkit
-
-def Alert1(texto):
-    account_sid = 'AC1c5692cfc9751a2fa7a7140edcbba0c7' 
-    auth_token = 'cf1d6295995e2d218d957e9e54d9e0e6'  
-    client = Client(account_sid, auth_token) 
-    message = client.messages.create( 
-                              from_='whatsapp:+14155238886',  
-                              body=texto,
-                              #media_url=''      
-                              to='whatsapp:+5492392537311') 
+ 
 def SaveImage(img):
     date = datetime.now()
     year_month = date.strftime('%Y-%m-%d,%H-%M-%S')
@@ -21,8 +11,7 @@ def SaveImage(img):
     return img_path
 
 def send_msj1(t, img_path):
-    pywhatkit.sendwhats_image(#phone_no='+5492392611662',
-                        phone_no='+5492392537311',
+    pywhatkit.sendwhats_image(phone_no='+5492392611662',
                         img_path= img_path,
                         caption= t,
                         tab_close=True)
@@ -32,7 +21,12 @@ def send_msj2(t, img_path):
                         caption= t,
                         tab_close=True)
 def send_msj3(t, img_path):
-    pywhatkit.sendwhats_image(phone_no='+5492396575139',
+    pywhatkit.sendwhats_image(phone_no='+5492392489549',
+                        img_path= img_path,
+                        caption= t,
+                        tab_close=True)
+def send_msj4(t, img_path):
+    pywhatkit.sendwhats_image(phone_no='+5492392616215',
                         img_path= img_path,
                         caption= t,
                         tab_close=True)
